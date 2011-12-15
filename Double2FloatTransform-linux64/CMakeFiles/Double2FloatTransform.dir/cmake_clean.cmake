70c6056041f8008a02aa02405dd5d364630e035e
Marion Hoogstoel <marion.hoogstoel@gmail.com>
2011-12-15 11:26:26 -0500

    D2FT with ANTS

---
+++ Double2FloatTransform-linux64/CMakeFiles/Double2FloatTransform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Double2FloatTransform.dir/Double2FloatTransform.cpp.o"
+  "Double2FloatTransform.pdb"
+  "Double2FloatTransform"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Double2FloatTransform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
